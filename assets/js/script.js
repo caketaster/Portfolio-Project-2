072da6ee91ce716b3da32a157b499ef9c33278d9
caketaster <>
2023-05-18 05:04:30 +0000

    create intro modal, tidy up html and css for question and answer areas, create one-question sample

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,47 @@
+/* Code to open the intro modal then select the quiz section */
+
+const modal = document.querySelector(".intro-modal");
+const overlay = document.querySelector(".overlay");
+let questionText = document.getElementsByClassName("questions");
+
+window.onload = introModal()
+function introModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+
+}
+
+function generalQs() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+function pgfQs() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    questionText.innerHTML = questions.PGF[0].question;
+};
+
+function scienceQs() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+
 /**
  * 
  * 
  */
 
-let questionText = document.getElementById("questions");
-questionText.innerHTML = /questions.json/PGF[0].question;
+
 
 
 /*
+let questionText = document.getElementsByClassName("questions")
+questionText.innerHTML = questions.PGF[0].question
 
-var correct = 0;
+
+var correct = 0; 
 var incorrect = 0;
 
 
@@ -26,12 +58,12 @@ function check(answer) {
 Correct answer should have an ID of 'correct/right' or similar so that I don't have to have a unique function run for each Q, how can I insert this into each question and answer?
 
 Where do I store the questions, answers and info text? In the HTML? In a separate text file (as I have now)? How do I make each Q show up sequentially?
---> questions inside an array..!
+--> questions inside an array..! 
 
 Answering (either correctly or wrongly) should bring up an information pop-up. Need to work out how to display this.
 
-Extras:
+Extras: 
 introduce each new section (Section 1 - general sasquatchery etc.) - need a pop-up for these
-insert pictures into info pop-ups?
+insert pictures into info pop-ups? 
 socials to have some cool graphics/animations??
 */
