0e94450999e004ccafa53fa0c820d69c3ef6ebfa
caketaster <>
2023-05-23 05:59:37 +0000

    Definte constants and variables for JS functions. Add event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,58 @@
-/* Code to open the intro modal then select the quiz section */
+// Define all constants needed
 
 const modal = document.querySelector(".intro-modal");
 const overlay = document.querySelector(".overlay");
 let questionText = document.getElementsByClassName("qtext");
 
-// window.onload = introModal() <-- don't need?
+const generalButton = document.querySelector('#btn-general');
+const pgfButton = document.querySelector('#btn-PGF');
+const scienceButton = document.querySelector('#btn-science');
+const qtext = document.querySelector("#qtext");
+const answerAText = document.querySelector("#answer-a");
+const answerBText = document.querySelector("#answer-b");
+const answerCText = document.querySelector("#answer-c");
+
+const buttonA = document.querySelector("#btn-A");
+const buttonB = document.querySelector("#btn-B");
+const buttonC = document.querySelector("#btn-C");
+
+const correctCountText = document.querySelector("#correct");
+const incorrectCountText = document.querySelector("#incorrect");
+
+const infoModal = document.querySelector("#infoModal");
+const infoText = document.querySelector('#info-text');
+
+const modalClose = document.querySelector("#modal-close");
+
+let questions = [];
+let index = 0;
+
+let correctCount = 0;
+let incorrectCount = 0;
+
+// Intro modal, info modal (display/hide)
 function introModal() {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 }
-// Call intro modal
+function displayInfoModal() {
+    infoModal.classList.remove("hidden");
+}
+function hideInfoModal() {
+    infoModal.classList.add("hidden");
+    goToNextQuestion();
+}
 introModal();
 
 //Listen for category button clicks
 generalButton.addEventListener("click", categoryClick);
 pgfButton.addEventListener('click', categoryClick);
 scienceButton.addEventListener('click', categoryClick);
+
+buttonA.addEventListener('click', checkAnswer);
+buttonB.addEventListener('click', checkAnswer);
+buttonC.addEventListener('click', checkAnswer);
+modalClose.addEventListener('click', hideInfoModal);
 
 
 function categoryClick(event) {
